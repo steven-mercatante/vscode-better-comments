51e4f022b4420217b1487d0d0b684f53fb018377
Steven Mercatante <steven.mercatante@gmail.com>
2019-09-03 14:06:49 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// regular comment
+// TODO: refactor
+// ! add unit test
+// ? should we rename this?
+// * useful note to colleagues
+// // another comment
+// @ custom comment
+function greet() {
+  return 'hello, viewer!'
+}
